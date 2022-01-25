376b3ba204eca2acf92da7afc83e58e5952f60b4
sindhuraman11 <sindhuraman2011@gmail.com>
2022-01-25 00:55:17 +0000

    hai

---
+++ src/main/java/gitdemo/app.java
@@ -0,0 +1,7 @@
+package gitdemo;
+
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Git is Cool");
+    }
+}
---
+++ src/test/java/appTest.java
@@ -0,0 +1,26 @@
+
+import org.junit.jupiter.api.Test;
+
+    public class appTest {
+
+        @Test
+        public void run1() {
+            System.out.println("The test1 is running");
+        }
+
+        @Test
+        public void run2() {
+            System.out.println("The test2 is running");
+        }
+
+        @Test
+        public void run3() {
+            System.out.println("The test3 is running");
+        }
+
+        @Test
+        public void run4() {
+            System.out.println("The test4 is running");
+        }
+    }
+
